b0bfd9803257b6c74fc2a251faf43f7807afe151
zjarlin <zjarlin@outlook.com>
2026-02-13 09:11:14 +0800

    Remove Android project and update KSP configuration.
    Removed the Android module and its associated resources, configurations, and dependencies due to project refocus. Updated KSP-related configurations and refined library declarations to align with the remaining structure.

--- openapi-codegen/build.gradle.kts
+++ openapi-codegen/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
-    id("site.addzero.buildlogic.kmp.kmp-core")
+    id("site.addzero.buildlogic.kmp.kmp-ksp")
 }
 kotlin{
-    jvm()
-
     dependencies {
-        implementation(libs.com.google.devtools.ksp.symbol.processing.api)
         implementation(libs.io.swagger.parser.v3.swagger.parser)
-        implementation(libs.com.squareup.kotlinpoet.v2)
+        implementation(libs.com.squareup.kotlinpoet)
         implementation(libs.com.squareup.kotlinpoet.ksp)
         testImplementation(libs.io.kotest.kotest.property)
         testImplementation(libs.io.kotest.kotest.assertions.core)
